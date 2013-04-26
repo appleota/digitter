c317320f4e3f50d6ffc1bb49a256468615a8e076
yablonsjkiy@gmail.com <yablonsjkiy@gmail.com>
2013-04-26 03:45:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digitter::Application.config.secret_token = '2d85c642c7dbe4d7b5eb57b10fccfccdf58a686f6bcc9aa1d1cde244ebb17bd0b09500951da1931b7d2d5ecbcae98e7f5fa2839782e7ea3250ff5a60a056c697'
